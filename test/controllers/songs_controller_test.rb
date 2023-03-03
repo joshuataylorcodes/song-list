b9988beb2e8fe0da5b0feed72107fc112ce19747
joshua <joshuamtaylor455@gmail.com>
2023-03-03 15:20:00 -0500

    add show test

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -9,6 +9,14 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
     assert_equal Song.count, data.length
   end
 
+  test "show" do
+    get "/songs/#{Song.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "album", "artist", "year", "created_at", "updated_at"], data.keys
+  end
+
   test "create" do
     assert_difference "Song.count", 1 do
       post "/songs.json", params: { name: "lake", width: 800, height: 600 }
