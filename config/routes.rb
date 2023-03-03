7b02bccd5a7918867afb82f4f2aa1271042dafa2
joshua <joshuamtaylor455@gmail.com>
2023-03-03 15:16:13 -0500

    add create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/songs" => "songs#index"
 
   get "/songs/:id" => "songs#show"
+
+  post "/songs" => "songs#create"
 end
--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -8,4 +8,11 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Song.count, data.length
   end
+
+  test "create" do
+    assert_difference "Song.count", 1 do
+      post "/songs.json", params: { name: "lake", width: 800, height: 600 }
+      assert_response 200
+    end
+  end
 end
